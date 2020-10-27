e568231d98faed58d00930136ddcd596ed2ce6b1
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 17:51:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require "yaml"
 def load_library(library)
   emoticon_array = YAML.load_file(library)
   emoticon_array.each_with_object({}) do |(key, value), final_array|
-    binding.pry
+    binding.pry!!
   end
   binding.pry
 end
