df1bd4bdae83a3ea57b0ac211df1dd2f7eebe8b8
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 18:27:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,7 +4,7 @@ require "yaml"
 
 def load_library(library)
   emoticon_array = YAML.load_file(library)
-  final_array = {}
+  output_array = {}
   emoticon_array.each do |key, value|
     final_array[key] = {}
     final_array[key][:english] = value[0]
