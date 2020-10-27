04e55fa1c60dcfb40439250efa48f58e951c4a13
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 17:58:07 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require "yaml"
 def load_library(library)
   emoticon_array = YAML.load_file(library)
   emoticon_array.each_with_object({}) do |(key, value), final_array|
-    final_array[key] = {:english, :japanese} 
+    final_array[key] = {} 
     binding.pry
   end
   binding.pry
