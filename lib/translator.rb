4858e5b8236072bd500fbb84c6f2e743aae5f6e4
Colton O'Connor <colton.shawn.oconnor@gmail.com>
2020-10-27 18:04:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(library)
   japanese = {}
   emoticon_array.each_with_object({}) do |(key, value), final_array|
     final_array[key] = {}
-    value.each do |inner_key, names|
+    value.each do |inner_key|
       binding.pry
     end
     binding.pry
